5161fc1d703c5ffce2763b72ceffe8dfdb4a8ae8
Serine Gheddou <ks_gheddou@esi.dz>
2022-12-24 18:23:29 +0100

    finalized the landing page style

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,47 +1,50 @@
-import { Button, Typography,Box } from "@mui/material";
-import { Container, display, flexbox } from "@mui/system";
+import { Button, Typography,Box, styled } from "@mui/material";
+import { useState } from "react";
 import HouseImg from "../media/For sale-bro.png";
 import "../Style/HeroStyle.css";
 
+
 const Hero = () =>{
+    const [clicked, setClicked]= useState(false);
+    function handleclick  (clicked) {
+     setClicked(!clicked);
+    }
+    
+    const AuthButton = styled(Button)(({theme}) => ({
+        backgroundColor: clicked ? "#202336" : "#16537e",
+        color:"white",
+        width:"200px",
+        marginBlock:"30px",
+        marginInlineEnd :"25px",
+        textAlign:"center",
+        paddingBlock:"10px",
+        borderRadius:"0px",
+        onClick : {handleclick},
+        "&:hover":{
+            backgroundColor:clicked ? "#202336" : "#16537e"
+        }
+        
+        
+   }));
     return (
         <Box  sx={{ 
             backgroundColor: "#E6F0FF", 
             height: '100vh',
             display:'flex',
             flexDirection:'row',
-            
             }}
             >
-            <Container className="container">
+            <div className="description">
                       <Typography variant="h3" >Find your perfect place with Bloom House</Typography>
                       <Typography >Adapt your house to your needs and make it heaven on earth.We provide a complet service for sale,
                          purchase and Rental of real estate
                       </Typography>
-                      <Container sx={{
-                        flexDirection:'row'
-                      }}>
-                        <Button sx={{
-                            backgroundColor:"#16537e",
-                            color:"white",
-                            margin:"20px"
-                            
-                        }}>
-                            s'inscrire
-                        </Button>
-                        <Button  sx={{
-                            backgroundColor:"#16537e",
-                            color:"white",
-                            margin:"20px",
-                            paddingLeft:"20px",
-                            paddingRight:"20px",
-                            paddingTop:"10px",
-                            paddingBottom:"10px"
-                        }}>
-                            se connecter
-                        </Button>
-                      </Container>  
-            </Container>
+                      <span>
+                        <AuthButton 
+                          onClick={handleclick}>s'inscrire</AuthButton>
+                        <AuthButton onClick={handleclick}>se connecter</AuthButton>
+                      </span>  
+            </div>
             <Box>
                 <img 
                   className="house-Img"
